eadfa9acbee6685ffe2344547e6dca1d6208c428
William Minghan Zhu <wzhu17@caddy14.stanford.edu>
2026-02-02 13:33:08 -0800

    clean sim directory before submission

--- make_submission.sh
+++ make_submission.sh
@@ -6,6 +6,10 @@ rm -rf $DIR $DIR.tar.gz
 mkdir -p $DIR
 cp README $DIR
 cp -R hw/hdl/verilog/mips $DIR
+cd sim/
+make clean
+cd ..
 cp -R sim/tests $DIR
 tar -czf $DIR.tar.gz $DIR
 rm -rf $DIR
+echo "Submission ready in $DIR.tar.gz"
